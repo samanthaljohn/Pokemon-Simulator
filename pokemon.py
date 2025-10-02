51bf541406c4f85d32c5170c19287f5497e0ca43
Samantha John <john.samanthalynn@gmail.com>
2025-10-02 13:41:24 -0600

    added str dunder function to pokemon class

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,23 @@
 class Pokemon:
-     def __innit__(self, name, type, stats):
+    def __innit__(self, name, type, stats, moves):
         """
         Initialize a Pokemon instance. 
 
         Parameters:
         name (str): Name of Pokemon.
         type (str): Pokemon Type.
-        stats (list): List of stats of Pokemon.
+        stats (list): List of Pokemon's moves.
+        moves (list): list of Pokemon's moves. 
         """
         self.name = name
         self.type = type
         self.stats = stats 
+        self.moves = moves
+
+    def __str__(self):
+        """
+        Returns a string with the following information:
+            - Pokemon's name
+            - Pokemon's type
+        """
+        return (f"Your Pokemon, {self.name}, is a {self.type} Pokemon!")
