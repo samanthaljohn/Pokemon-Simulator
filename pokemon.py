d96e3aaab51c5713309da8808f0a1845d3dade25
Samantha John <john.samanthalynn@gmail.com>
2025-10-02 22:40:48 -0600

    added natures constant list, created function to create your own pokemon.

--- pokemon.py
+++ pokemon.py
@@ -1,14 +1,29 @@
+import random
+
+#: List of the 25 possible Pokémon natures.
+#: Used to randomly assign a nature to each Pokemon.
+NATURES = ["Adamant", "Bashful", "Bold", "Brave", "Calm"
+        "Careful", "Docile", "Gentle", "Hardy", "Hasty", 
+        "Impish", "Jolly", "Lax", "Lonely", "Mild",
+        "Modest", "Naive", "Naughty", "Quiet", "Quirky",
+        "Rash", "Relaxed", "Sassy", "Serious", "Timid"]
+
 class Pokemon:
-    def __innit__(self, name, type1, type2, stats, moves):
+    """ 
+    Represents a Pokemon with stats, moves, typing, and nature.
+    Each Pokemon instance is assigned a random nature from `NATURES`.
+    """
+
+    def __innit__(self, name, type1, stats, moves, type2 = None):
         """
         Initialize a Pokemon instance. 
 
         Parameters:
         name (str): Name of Pokemon.
         type1 (str): Pokemon Type.
-        type2 (str): Pokemon secondary type.
         stats (list): List of Pokemon's moves.
         moves (list): list of Pokemon's moves. 
+        type2 (str): Pokemon secondary type - defaults to None.
         """
         self.name = name
         self.type1 = type1
@@ -23,3 +38,14 @@ class Pokemon:
             - Pokemon's type
         """
         return (f"Your Pokemon, {self.name}, is a {self.type1} Pokemon!")
+    
+    def create_mon(self, poke_dict):
+        """
+        Creates a Pokemon instance from an already constructed dictionary after checking 
+        that the right attributes are present.
+
+        Parameters:
+        poke_dict (dict): dictionary representing a pokemon
+        """
+
+        return Pokemon()
